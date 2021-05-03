22a2a2b2de4eb28f7a16ba40f1c139460eaa7dc8
tripleslicer <williamrevans@gmail.com>
2021-05-02 18:52:26 -0700

    create react app from scratch

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>JSX Challenge</h1>
+    <ul>
+      <li>Challenge 1</li>
+      <li>Challenge 2</li>
+      <li>Challenge 3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
